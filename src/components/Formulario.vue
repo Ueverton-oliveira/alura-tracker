5c86815f536bd9b157f680696c95efd150e3ff67
Ueverton-oliveira <ueverton.souz@gmail.com>
2022-11-25 16:58:37 -0400

    feat: props

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -6,10 +6,11 @@
             type="text"
             class="input"
             placeholder="Qual tarefa você deseja iniciar?"
+            v-model="descricao"
         />
       </div>
       <div class="columns">
-        <Temporizador />
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -20,7 +21,21 @@ import { defineComponent } from "vue";
 import Temporizador from "@/components/Temporizador.vue";
 export default {
   name: 'Formulario',
-  components: {Temporizador}
+  components: {
+    Temporizador
+  },
+  data () {
+    return {
+      descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa (tempoDecorrido: number) :void {
+      console.log('tempo da tarefa', tempoDecorrido)
+      console.log('descriçaõ da tarefa', this.descricao)
+      this.descricao = ''
+    }
+  }
 }
 
 </script>
